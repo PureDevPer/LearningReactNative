78425177133806234a2fcbd52da695a9458e2596
PureDevPer <wooseok.kim1207@gmail.com>
2019-04-17 00:52:40 -0400

    Weather app

---
+++ App.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from "./Weather";
+
+const API_KEY = "85616b14bfc84b9641e418e55ab915b3";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null,
+    cityName: null
+  }
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition( 
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+
+      error => {
+        this.setState({
+          error: error
+        })
+      }
+      
+    );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      console.log(json.name);
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        cityName: json.name,
+        isLoaded: true
+      });
+    });
+  }
+
+  render() {
+    const { isLoaded, error, temperature, name, cityName } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          //<Weather weatherName={"Mist"} temp ={Math.round(temperature)} />
+          <Weather weatherName={name} temp ={Math.round(temperature)} cityName={cityName} />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  errorText:{
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
+  },
+  loading:{
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100
+  }
+});
